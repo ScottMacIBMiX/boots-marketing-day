8e3d93a2966debebc5ca85f0b5e6723caba6f13d
Scott Hughes <Scott.Hughes1@ibm.com>
2025-01-23 12:25:04 +0000

    add awaiting page

---
+++ src/app/info/awaiting-results/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import "./results-page.scss";
+import Header from "@/Components/Header/Header";
+import Tabs from "@/Components/Tabs/Tabs";
+
+export default function Home() {
+  return (
+    <main className="results-main">
+      <Header />
+      <div className="tabs-container">
+        <Tabs />
+      </div>
+      <div className="results-content">
+        <div className="results-content__top">
+          <div className="title_vote">
+            <h1>Awaiting Results</h1>
+          </div>
+          <div className="info_vote">
+            <h1>
+             Please check back in a few minutes the votes are still being collected. In the meantime why not take a look at some of our <a href="https://www.ibm.com/blog/category/client-stories/">IBM Client Stories</a>?
+            </h1>
+          </div>
+          </div>
+      </div>
+    </main>
+  );
+}
