0bd50cd1c8bf4433b0816ffdcc1c5aefa2a2ecaa
Scott Hughes <Scott.Hughes1@ibm.com>
2025-01-23 12:07:46 +0000

    feat: hide airtable keys

--- src/utils/airtable.ts
+++ src/utils/airtable.ts
@@ -1,7 +1,7 @@
 import Airtable from "airtable";
 
 const base = new Airtable({
-  apiKey: 'pat9g9mrNARPfprMc.a9b43a782db29c19ed66f7012f95b43a6d3f3a616317eb6a08e502efcdef047c',
-}).base('appcN46pE7fr4ihEK');
+  apiKey: process.env.AIRTABLE_ACCESS_TOKEN,
+}).base(process.env.AIRTABLE_BASE_ID!);
 
 export default base;
